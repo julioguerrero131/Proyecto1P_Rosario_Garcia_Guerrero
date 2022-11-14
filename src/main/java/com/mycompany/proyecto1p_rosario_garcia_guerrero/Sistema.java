3cb73b3b52109980277157f7b1475bcb836b5984
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2022-11-13 23:29:05 -0500

    anadir clase Sistema

---
+++ src/main/java/com/mycompany/proyecto1p_rosario_garcia_guerrero/Sistema.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyecto1p_rosario_garcia_guerrero;
+
+/**
+ *
+ * @author julio
+ */
+public class Sistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
