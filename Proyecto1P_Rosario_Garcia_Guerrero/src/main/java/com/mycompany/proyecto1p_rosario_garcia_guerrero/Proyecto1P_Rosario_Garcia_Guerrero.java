54c8dc7b2d3014e4311aca89a3093f7662a3551c
July <juliodga12@gmail.com>
2022-11-13 22:03:49 -0500

    proyecto

---
+++ Proyecto1P_Rosario_Garcia_Guerrero/src/main/java/com/mycompany/proyecto1p_rosario_garcia_guerrero/Proyecto1P_Rosario_Garcia_Guerrero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto1p_rosario_garcia_guerrero;
+
+/**
+ *
+ * @author julio
+ */
+public class Proyecto1P_Rosario_Garcia_Guerrero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
